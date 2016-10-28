01a736b668414c94e3e6c4aa53f5e09bab7768e5
Brian Fannin <FanninQED@Yahoo.com>
2016-10-27 21:15:35 -0400

    Add grade factor

--- CleanDisag.R
+++ CleanDisag.R
@@ -5,10 +5,20 @@ library(tidyr)
 con <- dbConnect(drv = SQLite(), dbname = "./cleaned/disag.db")
 dfDisag <- dbGetQuery(con, "SELECT * FROM disag")
 
+mojo <- dfDisag$grade
+table(mojo)
+
+mojo <- factor(mojo, levels = c('03', '04' ,'05', '06', '07', '08'
+                                , 'EOC', 'GS', 'ALL')
+               , ordered = TRUE)
+
 dfDisag <- dfDisag %>% 
   mutate(num_glp = as.numeric(num_glp)
          , pct_glp = num_glp / num_tested
-         , SchoolYearInt = as.integer(substr(school_year, 1, 4)))
+         , SchoolYearInt = as.integer(substr(school_year, 1, 4))
+         , gradeFactor = factor(grade, levels = c('03', '04' ,'05', '06', '07', '08'
+                                                  , 'EOC', 'GS', 'ALL')
+                                , ordered = TRUE))
 
 # dfDisagWide <- dfDisag %>% 
 #   filter(school_code == '00A000') %>% 
@@ -16,8 +26,14 @@ dfDisag <- dfDisag %>%
 #   spread(subgroup, pct_glp)
 
 dfSchool <- dfDisag %>% 
-  select(school_code) %>% 
+  select(school_code, name) %>% 
+  group_by(school_code) %>% 
+  slice(1) %>% 
   unique()
+
+if (length(unique(dfSchool$school_code)) != nrow(dfSchool)) {
+  warning("Unique school codes don't equal records in school table.")
+}
 
 dfSchoolYear <- dfDisag %>% 
   select(school_code, school_year) %>% 
@@ -51,7 +67,7 @@ dfEOG <- dfDisag %>%
   filter(subject == "EOG"
          , subgroup == "ALL"
          , !is.na(pct_glp)) %>% 
-  select(school_code, school_year, type, grade, SchoolYearInt, num_tested, num_glp, pct_glp) %>% 
+  select(school_code, school_year, type, grade, gradeFactor, SchoolYearInt, num_tested, num_glp, pct_glp) %>% 
   left_join(dfSchoolComposition)
 
 save(file = "disag.rda"
